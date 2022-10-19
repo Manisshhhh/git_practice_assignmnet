489c1f96f0215a66dda5b4b570f6d121787ea449
Moumita-Jana <moumitajana1004@gmail.com>
2022-10-20 00:55:09 +0530

    new_updated_added

--- palindrome_problem.js
+++ palindrome_problem.js
@@ -2,8 +2,15 @@ PROBLEM 2 : PALINDROME PROBLEM...
  let str = madam;
  let N = 5;
  let bag = "";
-    
-        bag = bag + str[i];
+for(i=(N-1);i>=0;i--){
+    bag=bag+str[i];
+    }
+    if(bag==str){
+       console.log("Yes");
+} 
+else{
+      console.log("No)
+}
     
 
  
--- prime_problems.js
+++ prime_problems.js
@@ -4,8 +4,8 @@ PROBLEM 1 : PRIME NUMBER...
 let num = 9;
 let count = 0;
 
-for (i = 1; i<=num; i--){
-         if ( num % i !==0){
+for (i = 1; i<=num-1; i++){
+         if ( num % i ==0){
           count++;
     }
       }
