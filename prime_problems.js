181ba1cbf6de7e8171649b3f98fe5dd40fdc3b59
Manisshhhh <manishvishwakarma0026@gmail.com>
2022-10-19 23:30:26 +0530

    Problem1

---
+++ prime_problems.js
@@ -0,0 +1,20 @@
+PROBLEM 1 : PRIME NUMBER...
+
+
+let num = 9;
+let count = 0;
+
+for (i = 1; i<=num; i--){
+         if ( num % i !==0){
+          count++;
+    }
+      }
+
+        if( count == 2){
+     console.log("YES");
+  }  
+        else {  
+      console.log("NO"); 
+  }
+ }
+ }
